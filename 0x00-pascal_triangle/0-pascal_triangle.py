1cc1751a7287672cb60d20e8c8cf448702d0adf6
Romaric250 <lonfonyuyromeo@gmail.com>
2024-01-03 17:30:18 -1200

    updated some content

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,32 @@
-def pascal_triangle(n):
-    if n <= 0:
-        return []
+#!/usr/bin/python3
+"""A script to get pascals triangle"""
 
-    triangle = [[1]]
-    for i in range(1, n):
-        previous_row = triangle[i-1]
-        current_row = [1]
-        for j in range(1, i):
-            current_row.append(previous_row[j-1] + previous_row[j])
-        current_row.append(1)
-        triangle.append(current_row)
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing the Pascal’s triangle of n.
+
+    Args:
+        n (int): The height of the Pascal's triangle.
+
+    Returns:
+        list: A list of lists representing Pascal's triangle.
+
+    """
+    triangle = []
+
+    if n <= 0:
+        return triangle
+
+    for i in range(n):
+        temp_list = []
+
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                temp_list.append(1)
+            else:
+                temp_list.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+
+        triangle.append(temp_list)
 
     return triangle
