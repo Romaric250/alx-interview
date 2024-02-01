dc4e82fd9aa34561d64cd03e0ea7e327fefa471e
Romaric250 <lonfonyuyromeo@gmail.com>
2024-02-01 22:22:40 +0100

    updated the code for more readability

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-UTF-8 Validation script
+UTF-8 Validation
 """
 
 
@@ -12,22 +12,19 @@ def validUTF8(data):
     Returns:
         - True if data is a valid UTF-8 encoding, else False
     """
-
+    # Number of bytes in the current UTF-8 character
     num_bytes = 0
 
     for num in data:
         # Check if the current number is a continuation byte
-        if num >> 6 == 0b10:
+        if num >> 6 != 0b10:
+            # If the current number is not a continuation byte but
+            # num_bytes is still greater than 0, it means that the
             # previous character was not properly terminated
-            if num_bytes == 0:
-                return False
-            num_bytes -= 1
-        else:
-            # Check the number of bytes in the current character
             if num_bytes != 0:
                 return False
 
-            # Get the number of bytes in the current UTF-8 character
+            # Check the number of bytes in the current character
             if num >> 7 == 0b0:
                 num_bytes = 0
             elif num >> 5 == 0b110:
@@ -37,6 +34,16 @@ def validUTF8(data):
             elif num >> 3 == 0b11110:
                 num_bytes = 3
             else:
+                return False
+        else:
+            # If the current number is a continuation byte, it should
+            # decrement the number of bytes remaining to complete the
+            # current character
+            num_bytes -= 1
+
+            # If num_bytes becomes negative, it means that there are
+            # more continuation bytes than expected
+            if num_bytes < 0:
                 return False
 
     # If there are remaining bytes to complete a character, return False
