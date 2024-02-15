d99472243e9cd317cf613b979ad2655a65db4fed
Romaric250 <lonfonyuyromeo@gmail.com>
2024-02-15 23:59:54 +0100

    starwars api implementation

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+
+const url = `https://swapi.dev/api/films/${movieId}/`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+  const film = JSON.parse(body);
+  const characters = film.characters;
+
+  characters.forEach((characterUrl) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        console.error('Error:', error);
+        return;
+      }
+      const character = JSON.parse(body);
+      console.log(character.name);
+    });
+  });
+});
