24e79f15aa7d35fe8e2227d4c493e66d7d6f2e41
Romaric250 <lonfonyuyromeo@gmail.com>
2024-03-05 05:50:33 +0100

    task completed

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,10 +3,12 @@
 This script calculates the perimeter of an island represented in a 2D grid.
 """
 
+
 def check_cell(cell_value):
     """
-    Check the value of a cell. Returns 1 if the cell is water (0), and 0 if the cell is land (1).
-    
+    Check the value of a cell. Returns 1 if
+    the cell is water (0), and 0 if the cell is land (1).
+
     Args:
         cell_value (int): The value of the cell (0 or 1).
 
@@ -19,9 +21,9 @@ def check_cell(cell_value):
 def island_perimeter(grid):
     """
     Calculate the perimeter of an island represented in a 2D grid.
-    
+
     Args:
-        grid (list of list of int): The 2D grid representing the island. 
+        grid (list of list of int): The 2D grid representing the island.
                                     1 represents land and 0 represents water.
 
     Returns:
@@ -35,21 +37,21 @@ def island_perimeter(grid):
         for col in range(num_cols):
             assert (grid[row][col] == 0) or (grid[row][col] == 1)
             if grid[row][col] == 1:
-                if row-1 < 0:
+                if row - 1 < 0:
                     perimeter += 1
                 else:
-                    perimeter += check_cell(grid[row-1][col])
-                if col-1 < 0:
+                    perimeter += check_cell(grid[row - 1][col])
+                if col - 1 < 0:
                     perimeter += 1
                 else:
-                    perimeter += check_cell(grid[row][col-1])
+                    perimeter += check_cell(grid[row][col - 1])
 
                 try:
-                    perimeter += check_cell(grid[row+1][col])
+                    perimeter += check_cell(grid[row + 1][col])
                 except IndexError:
                     perimeter += 1
                 try:
-                    perimeter += check_cell(grid[row][col+1])
+                    perimeter += check_cell(grid[row][col + 1])
                 except IndexError:
                     perimeter += 1
 
